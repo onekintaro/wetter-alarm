1c3e8b0bc399f75e073a774b68661ae6c9a2643f
onekintaro <kintaro221@gmail.com>
2024-06-22 01:30:28 +0200

    chore: Add cantons list to Wetter-Alarm const file

--- custom_components/wetter_alarm/const.py
+++ custom_components/wetter_alarm/const.py
@@ -10,3 +10,33 @@ REGION = "region"
 TITLE = "title"
 HINT = "hint"
 SIGNATURE = "signature"
+
+CANTONS = [
+    {"name": "Aargau", "code": "AG"},
+    {"name": "Appenzell Innerrhoden", "code": "AI"},
+    {"name": "Appenzell Ausserrhoden", "code": "AR"},
+    {"name": "Bern", "code": "BE"},
+    {"name": "Basel-Landschaft", "code": "BL"},
+    {"name": "Basel-Stadt", "code": "BS"},
+    {"name": "Freiburg", "code": "FR"},
+    {"name": "Genf", "code": "GE"},
+    {"name": "Glarus", "code": "GL"},
+    {"name": "Graubünden", "code": "GR"},
+    {"name": "Jura", "code": "JU"},
+    {"name": "Luzern", "code": "LU"},
+    {"name": "Neuenburg", "code": "NE"},
+    {"name": "Nidwalden", "code": "NW"},
+    {"name": "Obwalden", "code": "OW"},
+    {"name": "St. Gallen", "code": "SG"},
+    {"name": "Schaffhausen", "code": "SH"},
+    {"name": "Solothurn", "code": "SO"},
+    {"name": "Schwyz", "code": "SZ"},
+    {"name": "Thurgau", "code": "TG"},
+    {"name": "Tessin", "code": "TI"},
+    {"name": "Uri", "code": "UR"},
+    {"name": "Waadt", "code": "VD"},
+    {"name": "Wallis", "code": "VS"},
+    {"name": "Zug", "code": "ZG"},
+    {"name": "Zürich", "code": "ZH"},
+    {"name": "Ohne Kanton", "code": None}
+]
