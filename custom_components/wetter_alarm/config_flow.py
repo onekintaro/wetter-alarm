a36972a7b439c3f568e52e2fcda8eecc80d25c5e
onekintaro <kintaro221@gmail.com>
2024-06-30 22:49:48 +0200

    chore: Update Wetter-Alarm config flow to include POI search functionality

--- custom_components/wetter_alarm/config_flow.py
+++ custom_components/wetter_alarm/config_flow.py
@@ -5,181 +5,355 @@ import json
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers.selector import TextSelector, TextSelectorConfig
-from homeassistant.helpers.translation import async_get_translations
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.json import JSONEncoder
+from homeassistant.helpers.selector import (
+    SelectSelector,
+    NumberSelector,
+    TextSelector,
+    LocationSelector,
+    )
 
 from . import WetterAlarmApiClient
-from .const import DOMAIN, CANTONS
+from .const import (
+    DOMAIN,
 
-_LOGGER = logging.getLogger(__name__)
+    CONF_WA_PREFIXES,
+
+    CONF_LANGUAGE,
+    CONF_LANGUAGE_LIST,
+    CONF_DEFAULT_LANGUAGE,
+
+    CONF_ERROR_NO_POI_FOUND,
+    CONF_ERROR_NO_CANTON,
+    CONF_ERROR_NO_POI_ID,
+    CONF_ERROR_INVALID_POI_ID,
+  
+    CONF_POI_DATA,
+    CONF_POI_LIST,
+    CONF_SET_POI_BY,
+    CONF_BY_ID,
+    CONF_BY_SEARCH,
+    CONF_BY_MAP,
+    CONF_POI_ID,
+    CONF_POI_MAP,
+    CONF_POI_NAME,
+
+    CONF_POI_SEARCH,
+    CONF_CANTON,
+    CONF_CANTONS,
+    )
 
 # Load POIs from JSON file
 with open('custom_components/wetter_alarm/pois.json', 'r') as file:
     POIS = json.load(file)
 
+_LOGGER = logging.getLogger(__name__)
+
 class WetterAlarmConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
-    async def canton_options(self):
-        translations = await async_get_translations(self.hass, 'strings', 'en')
-        options = {"": translations.get("config.canton.placeholder", "Please select a canton")}
-        options.update({canton['code']: translations.get(f"config.canton.{canton['code']}", canton['name']) for canton in CANTONS})
-        return options
-
     async def async_step_user(self, user_input=None):
         errors = {}
-        if user_input is not None:
-            if user_input["selection"] == "by_id":
-                return await self.async_step_enter_id()
-            elif user_input["selection"] == "by_search":
-                return await self.async_step_search()
 
-        translations = await async_get_translations(self.hass, 'strings', 'en')
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
+        if user_input is not None:
+            self.context[CONF_LANGUAGE] = user_input[CONF_LANGUAGE]
+
+            if user_input[CONF_SET_POI_BY] == CONF_BY_ID:
+                return await self.async_step_enter_id()
+            elif user_input[CONF_SET_POI_BY] == CONF_BY_SEARCH:
+                return await self.async_step_search()
+            elif user_input[CONF_SET_POI_BY] == CONF_BY_MAP:
+                return await self.async_step_map()
+
+        data_options = [
+            CONF_BY_ID,
+            CONF_BY_SEARCH,
+            #TODO: Implement map selection, DISABLING FOR NOW
+            #CONF_BY_MAP,
+        ]
+        data_schema=vol.Schema(
                 {
-                    vol.Required("selection"): vol.In({
-                        "by_id": translations.get("config.selection.by_id", "Enter POI ID"),
-                        "by_search": translations.get("config.selection.by_search", "Search by Canton and Location")
+                    vol.Required(
+                        CONF_LANGUAGE,
+                        default=CONF_DEFAULT_LANGUAGE
+                    ): SelectSelector({
+                        "options": list(CONF_LANGUAGE_LIST),
+                        "custom_value": False,
+                        "mode": "dropdown",
+                        "translation_key": CONF_LANGUAGE
+                    }),
+                    vol.Required(
+                        CONF_SET_POI_BY,
+                        default=CONF_BY_SEARCH
+                    ): SelectSelector({
+                        "options": list(data_options),
+                        "custom_value": False,
+                        "mode": "list",
+                        "translation_key": CONF_SET_POI_BY
                     })
                 }
-            ),
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
             errors=errors,
             description_placeholders={
-                "title": translations.get("config.step.user.title", "Wetter-Alarm Setup"),
-                "description": translations.get("config.step.user.description", "Choose how you want to set up the POI.")
+                "title": "title",
+                "description": "description"
             }
         )
 
     async def async_step_enter_id(self, user_input=None):
         errors = {}
-        translations = await async_get_translations(self.hass, 'strings', 'en')
         if user_input is not None:
-            poi_id = user_input.get("poi_id")
+            poi_id = user_input.get(CONF_POI_ID)
             poi = next((poi for poi in POIS if poi['poiID'] == poi_id), None)
 
-            if poi:
-                return await self.async_step_name({
-                    "poi_id": poi['poiID'],
-                    "poi_name": poi['label_de']
-                })
+            if poi_id == 0:
+                errors["base"] = CONF_ERROR_NO_POI_ID
+            elif poi:
+                self.context[CONF_POI_DATA] = poi
+                return await self.async_step_name()
             else:
-                errors["base"] = "config.error.invalid_poi_id"
+                errors["base"] = CONF_ERROR_INVALID_POI_ID
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_POI_ID,
+                    default="0"
+                ): NumberSelector({
+                    "min": 0,
+                    "max": 999999,
+                    "step": 1,
+                    "mode": "box",
+                })
+            }
+        )
 
         return self.async_show_form(
             step_id="enter_id",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("poi_id"): int,
-                }
-            ),
+            data_schema=data_schema,
             errors=errors,
             description_placeholders={
-                "title": translations.get("config.step.enter_id.title", "Enter POI ID"),
-                "description": translations.get("config.step.enter_id.description", "Please enter the POI ID.")
+                "title": "title",
+                "description": "description" 
             }
         )
 
     async def async_step_search(self, user_input=None):
         errors = {}
-        translations = await async_get_translations(self.hass, 'strings', 'en')
         if user_input is not None:
-            selected_canton = user_input.get("canton")
-            poi_search = user_input.get("poi_search", "").lower()
+            lang = self.context.get(CONF_LANGUAGE)
+            selected_canton = user_input.get(CONF_CANTON)
+            poi_search = user_input.get(CONF_POI_SEARCH, "").lower()
+            matching_pois = []
 
-            # Filter POIs based on selected canton and search input
-            matching_pois = [
-                poi for poi in POIS
-                if (selected_canton == "Ohne Kanton" and poi['canton'] is None) or
-                   (poi['canton'] == selected_canton and (
-                    poi_search in poi['label_de'].lower() or
-                    poi_search in poi['label_fr'].lower() or
-                    poi_search in poi['label_it'].lower() or
-                    poi_search in poi['label_en'].lower()
-                ))
-            ]
-
-            if len(matching_pois) == 1:
-                return await self.async_step_name({
-                    "poi_id": matching_pois[0]['poiID'],
-                    "poi_name": matching_pois[0]['label_de']
-                })
-            elif len(matching_pois) > 1:
-                return await self.async_step_select_poi({
-                    "canton": selected_canton,
-                    "matching_pois": matching_pois
-                })
+            if selected_canton == "none" and poi_search == "":
+                errors["base"] = CONF_ERROR_NO_CANTON
             else:
-                errors["base"] = "config.error.no_pois_found"
+                if selected_canton == "none" and poi_search != "":
+                    for poi in POIS:
+                        poi_name = poi[lang]
+                        poi_parent_name = poi.get(f"parent_{lang}")
+                        if poi_parent_name:
+                            poi_name = f"{poi_parent_name}, {poi_name}"
+                        
+                        if poi_search in poi_name.lower():
+                            poi['name'] = poi_name
+                            matching_pois.append(poi)
+                else:
+                    for poi in POIS:
+                        if selected_canton == "other" and poi['canton'] is None:
+                            poi_name = poi[lang]
+                            poi_parent_name = poi.get(f"parent_{lang}")
+                            if poi_parent_name:
+                                poi_name = f"{poi_parent_name}, {poi_name}"
+                            
+                            if poi_search in poi_name.lower():
+                                matching_pois.append(poi)
+                                
+                        elif poi['canton'] == selected_canton:
+                            poi_name = poi[lang]
+                            poi_parent_name = poi.get(f"parent_{lang}")
+                            if poi_parent_name:
+                                poi_name = f"{poi_parent_name}, {poi_name}"
+                            
+                            if poi_search in poi_name.lower():
+                                poi['name'] = poi_name
+                                matching_pois.append(poi)
+                    
+
+                if len(matching_pois) == 1:
+                    self.context[CONF_POI_DATA] = matching_pois[0]
+                    return await self.async_step_name()
+                elif len(matching_pois) > 1:
+                    matching_pois.sort(key=lambda poi: poi['name'])
+                    self.context[CONF_POI_LIST] = matching_pois
+                    return await self.async_step_select_poi()
+                else:
+                    errors["base"] = CONF_ERROR_NO_POI_FOUND
+
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_CANTON,
+                    default="none"
+                ): SelectSelector({
+                    "options": list(CONF_CANTONS),
+                    "custom_value": False,
+                    "mode": "dropdown",
+                    "translation_key": CONF_CANTON
+                }),
+                vol.Optional(
+                    CONF_POI_SEARCH,
+                    default=""
+                ): TextSelector({})
+            }
+        )
 
         return self.async_show_form(
             step_id="search",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("canton"): vol.In(await self.canton_options()),
-                    vol.Optional("poi_search"): TextSelector(TextSelectorConfig(type="text")),
-                }
-            ),
+            data_schema=data_schema,
             errors=errors,
             description_placeholders={
-                "title": translations.get("config.step.search.title", "Search by Canton and Location"),
-                "description": translations.get("config.step.search.description", "Select a canton and optionally enter a location to search for POIs.")
+                "title": "title",
+                "description": "description"
             }
         )
-
+    
     async def async_step_select_poi(self, user_input=None):
         errors = {}
-        translations = await async_get_translations(self.hass, 'strings', 'en')
-        matching_pois = user_input.get("matching_pois", [])
+        matching_pois = self.context.get(CONF_POI_LIST)
+        lang = self.context.get(CONF_LANGUAGE)
 
-        if user_input is not None and "poi_id" in user_input:
-            selected_poi_id = user_input.get("poi_id")
+        if user_input is not None and CONF_POI_ID in user_input:
+            selected_poi_id = user_input.get(CONF_POI_ID)
             selected_poi = next(poi for poi in POIS if poi['poiID'] == int(selected_poi_id))
-            return await self.async_step_name({
-                "poi_id": selected_poi['poiID'],
-                "poi_name": selected_poi['label_de']
-            })
+            self.context[CONF_POI_DATA] = selected_poi
+            return await self.async_step_name()
 
-        options = {str(poi['poiID']): f"{poi['label_de']} ({poi['canton']})" for poi in matching_pois}
+        options = []
+        for poi in matching_pois:
+            poi_name = poi[lang]
+            poi_parent_name = poi.get(f"parent_{lang}")
+            if poi_parent_name:
+                poi_name = f"{poi_parent_name}, {poi_name}"
+            options.append({"label": f"{poi_name} ({poi['canton']})", "value": str(poi['poiID'])})
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_POI_ID
+                ): SelectSelector({
+                    "options": options,
+                    "custom_value": False,
+                    "mode": "dropdown",
+                    "sort": True,
+                    "translation_key": CONF_POI_ID
+                })
+            }
+        )
 
         return self.async_show_form(
             step_id="select_poi",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("poi_id"): vol.In(options),
-                }
-            ),
+            data_schema=data_schema,
             errors=errors,
             description_placeholders={
-                "title": translations.get("config.step.select_poi.title", "Select POI"),
-                "description": translations.get("config.step.select_poi.description", "Multiple POIs found. Please select one.")
+                "title": "title",
+                "description": "description"
+            }
+        )
+    
+    async def async_step_map(self, user_input=None):
+        _LOGGER.error("Entered map step")
+        errors = {}
+        if user_input is not None:
+            # Hier kannst du die Benutzereingaben verarbeiten und passende POIs finden
+            location = user_input.get(CONF_POI_MAP)
+            radius = location.get("radius")
+            lat = location.get("latitude")
+            lon = location.get("longitude")
+
+            # Implementiere die Logik, um POIs basierend auf der Karte zu finden
+            matching_pois = [
+                poi for poi in POIS
+                if self._is_within_radius(poi['lat'], poi['long'], lat, lon, radius)
+            ]
+
+            if len(matching_pois) == 1:
+                self.context[CONF_POI_DATA] = matching_pois[0]
+                return await self.async_step_name()
+            elif len(matching_pois) > 1:
+                self.context[CONF_POI_LIST] = matching_pois
+                return await self.async_step_select_poi()
+            else:
+                errors["base"] = CONF_ERROR_NO_POI_FOUND
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_POI_MAP
+                ): LocationSelector({
+                    "radius": True,
+                })
             }
         )
 
+        return self.async_show_form(
+            step_id="map",
+            data_schema=data_schema,
+            errors=errors,
+            description_placeholders={
+                "title": "title",
+                "description": "description"
+            }
+        )
+
+
     async def async_step_name(self, user_input=None):
-        translations = await async_get_translations(self.hass, 'strings', 'en')
         if user_input is not None:
-            poi_id = user_input.get("poi_id")
-            poi_name = user_input.get("poi_name")
-            custom_name = user_input.get("custom_name") or poi_name
+            poi_data = self.context.get(CONF_POI_DATA)
+            poi_name = user_input.get(CONF_POI_NAME)
+            
+            _LOGGER.error(f"POI data: {poi_data}")
 
             return self.async_create_entry(
-                title=f"{custom_name}",
-                data={"pois": [(custom_name, poi_id)]},
+                title=f"{poi_name}",
+                data={"pois": [(poi_name, poi_data)]},
             )
+        
+        poi_data = self.context.get(CONF_POI_DATA)
+        lang = self.context.get(CONF_LANGUAGE)
+        poi_name = poi_data.get(lang)
+        poi_parent_name = poi_data.get(f"parent_{lang}")
+
+        if poi_parent_name:
+            poi_name = f"{poi_parent_name}, {poi_name}"
+
+        #add Prefix to poi_name
+        poi_name_full = f"{CONF_WA_PREFIXES[lang]}{poi_name} ({poi_data['canton']})"
+        
+        
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_POI_NAME,
+                    default=poi_name_full
+                ): TextSelector({}),
+            }
+        )
 
         return self.async_show_form(
             step_id="name",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("poi_name"): str,
-                    vol.Optional("custom_name"): str,
-                }
-            ),
+            data_schema=data_schema,
             description_placeholders={
-                "title": translations.get("config.step.name.title", "Set POI Name"),
-                "description": translations.get("config.step.name.description", "You can give the POI a custom name.")
+                "poi_name": poi_name,
+                "title": "title",
+                "description": "description"
             }
         )
