e087b454a24d8e0f9a73943d6c58fdb49f0ac139
onekintaro <kintaro221@gmail.com>
2024-06-30 22:50:16 +0200

    chore: Update Wetter-Alarm const file with cantons list and add POI search functionality to config flow

--- custom_components/wetter_alarm/const.py
+++ custom_components/wetter_alarm/const.py
@@ -8,35 +8,69 @@ VALID_TO = "valid_to"
 PRIORITY = "priority"
 REGION = "region"
 TITLE = "title"
-HINT = "hint"
+HINT = "description"
 SIGNATURE = "signature"
 
-CANTONS = [
-    {"name": "Aargau", "code": "AG"},
-    {"name": "Appenzell Innerrhoden", "code": "AI"},
-    {"name": "Appenzell Ausserrhoden", "code": "AR"},
-    {"name": "Bern", "code": "BE"},
-    {"name": "Basel-Landschaft", "code": "BL"},
-    {"name": "Basel-Stadt", "code": "BS"},
-    {"name": "Freiburg", "code": "FR"},
-    {"name": "Genf", "code": "GE"},
-    {"name": "Glarus", "code": "GL"},
-    {"name": "Graubünden", "code": "GR"},
-    {"name": "Jura", "code": "JU"},
-    {"name": "Luzern", "code": "LU"},
-    {"name": "Neuenburg", "code": "NE"},
-    {"name": "Nidwalden", "code": "NW"},
-    {"name": "Obwalden", "code": "OW"},
-    {"name": "St. Gallen", "code": "SG"},
-    {"name": "Schaffhausen", "code": "SH"},
-    {"name": "Solothurn", "code": "SO"},
-    {"name": "Schwyz", "code": "SZ"},
-    {"name": "Thurgau", "code": "TG"},
-    {"name": "Tessin", "code": "TI"},
-    {"name": "Uri", "code": "UR"},
-    {"name": "Waadt", "code": "VD"},
-    {"name": "Wallis", "code": "VS"},
-    {"name": "Zug", "code": "ZG"},
-    {"name": "Zürich", "code": "ZH"},
-    {"name": "Ohne Kanton", "code": None}
+CONF_LANGUAGE = "poi_language"
+CONF_DEFAULT_LANGUAGE = "label_de"
+CONF_LANGUAGE_LIST = [
+    "label_de",
+    "label_fr",
+    "label_it",
+    "label_en"
+]
+
+CONF_WA_PREFIXES = {
+    "label_de": "Wetter-Alarm: ",
+    "label_fr": "Alarme-Météo: ",
+    "label_it": "Allarme-Meteo: ",
+    "label_en": "Weather-Alarm: "
+}
+
+CONF_ERROR_NO_POI_FOUND = "error_no_poi_found"
+CONF_ERROR_NO_CANTON = "error_no_canton"
+CONF_ERROR_NO_POI_ID = "error_no_poi_id"
+CONF_ERROR_INVALID_POI_ID = "error_invalid_poi_id"
+
+CONF_POI_ID = "poi_id"
+CONF_POI_MAP = "poi_map"
+CONF_POI_NAME = "poi_name"
+CONF_POI_DATA = "poi_data"
+CONF_POI_LIST = "poi_list"
+CONF_SET_POI_BY = "set_poi_by"
+CONF_BY_ID = "set_by_id"
+CONF_BY_SEARCH = "set_by_search"
+CONF_BY_MAP = "set_by_map"
+CONF_POI_SEARCH = "poi_search"
+
+CONF_CANTON = "canton"
+CONF_CANTONS = [
+    "none",
+    "AG",
+    "AI",
+    "AR",
+    "BE",
+    "BL",
+    "BS",
+    "FR",
+    "GE",
+    "GL",
+    "GR",
+    "JU",
+    "LU",
+    "NE",
+    "NW",
+    "OW",
+    "SG",
+    "SH",
+    "SO",
+    "SZ",
+    "TG",
+    "TI",
+    "UR",
+    "VD",
+    "VS",
+    "ZG",
+    "ZH",
+    "other"
 ]
