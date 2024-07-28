2fc30fd7b3c28430046849c12d44f76e1f2476bd
onekintaro <kintaro221@gmail.com>
2024-07-28 14:40:12 +0200

    Rework: Coordinator Fix Issue: #1 (Thanks to @Lutehi-Gregor) in Coordinator (should now Update the Data)

--- custom_components/wetter_alarm/coordinator.py
+++ custom_components/wetter_alarm/coordinator.py
@@ -1,45 +1,97 @@
 from datetime import timedelta, datetime, timezone
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
 import logging
 
-from .wetter_alarm_client import WetterAlarmApiClient
+from .client import WetterAlarmApiClient
+from .const import DOMAIN
+from .consts.config import Config as Cfg
+from .consts.options import OptionConsts as Opt, Options
+from .consts.devices import Device, DeviceConst
 
-#SCAN_INTERVAL = timedelta(minutes=30)
+_LOGGER = logging.getLogger(__name__)
 
 class WetterAlarmCoordinator(DataUpdateCoordinator):
     """Custom Wetter-Alarm Coordinator"""
 
-    def __init__(self, hass: HomeAssistant, logger: logging.Logger, config_entry, poi_id: int, poi_name: str) -> None:
-        self._hass = hass
-        self._poi_id = poi_id
-        self._name = poi_name
-        self._logger = logger
+    def __init__(self, hass: HomeAssistant, config_entry) -> None:
+        self._poi_id = config_entry.data[Cfg.POI.ID]
+        self._name = Opt.getName(config_entry.options)
+        self._logger = _LOGGER
         self.last_update = None
         self._config_entry = config_entry
+        
+        Device.setConf(config_entry.options)
+        self._devices = Device.getEntries()
 
-        update_interval = timedelta(minutes=config_entry.options.get('api_update_interval', 30))
+        update_interval = Opt.getUpdateInterval(config_entry.options)
+        #_LOGGER.error(f"Update interval set to: {update_interval}")
 
         super().__init__(
             hass=hass,
-            logger=logger,
+            logger=_LOGGER,
             name=self._name,
             update_interval=update_interval,
         )
 
-    @property
-    def get_hass(self):
-        return self._hass
+    def get_device_info(self, device_name: str) -> DeviceInfo:
+        """Return the device info based on device name."""
+        device_info = self._devices.get(device_name, {})
+        if device_info and device_name != DeviceConst.HUB:
+            return DeviceInfo(
+                identifiers={(DOMAIN, f"{self._poi_id}-{device_name}")},
+                translation_key=device_name,
+                translation_placeholders={"poi_name_short": self._name},
+                manufacturer="Wetter-Alarm",
+                model="API v8",
+                entry_type="service",
+                via_device=(DOMAIN, f"{self._poi_id}-hub") if device_info['active'] else None
+            )
+        return self.get_hub_device_info()
 
-    @property
-    def get_poi_id(self):
-        return self._poi_id
+    def get_hub_device_info(self) -> DeviceInfo:
+        """Return the device info for the city."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, f"{self._poi_id}-hub")},
+            translation_key=DeviceConst.HUB,
+            translation_placeholders={"poi_name_short": self._name},
+            manufacturer="Wetter-Alarm",
+            model="API v8",
+            entry_type="service"
+        )
 
     async def _async_update_data(self) -> dict:
         """Fetch data from API endpoint."""
-        client = WetterAlarmApiClient(self._poi_id, self._config_entry)
-        alert_data = await client.search_for_alerts_async(hass=self._hass)
-        weather_data = await client.get_weather_data_async(hass=self._hass)
+        client = WetterAlarmApiClient(self._config_entry)
+
+        alert_data = await client.get_meteo_alerts_async(self.hass)
+        weather_data = await client.get_weather_data_async(self.hass)
+        info_data = await client.get_poiinfo_data_async(self.hass)
         self.last_update = datetime.now(timezone.utc)
-        self._logger.debug(f"Data fetched at {self.last_update}: {alert_data}, {weather_data}")
-        return {**alert_data, **weather_data, 'last_update': self.last_update}
+
+        api_data = {
+            'devices': self._devices,
+            'meteo_alerts': alert_data,
+            'weather_data': weather_data,
+            'info_data': info_data,
+            'last_update': self.last_update
+        }
+
+        # _LOGGER.error("Device data: %s", self._devices)
+        # _LOGGER.error("Alert data: %s", alert_data)
+        # _LOGGER.error("Weather data: %s", weather_data)
+        # _LOGGER.error("Info data: %s", info_data)
+        # _LOGGER.error("Last update: %s", self.last_update)
+
+        # _LOGGER.error("Data: %s", api_data)
+
+
+        return api_data
+
+    def get_alert_count(self, alert_list) -> int:
+        """Return the number of alerts in the list."""
+        length = len(alert_list)
+        if length == 1 and alert_list[0].get('type') == 'no_alerts':
+            return 0
+        return length
