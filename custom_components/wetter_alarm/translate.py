2ff9848d25842ee7a23bd8582ba919f8fa08b105
onekintaro <kintaro221@gmail.com>
2024-07-28 14:43:15 +0200

    Fix: Translate Helper (i should move it to helpers folder 😅, will be fixed in next version)

--- custom_components/wetter_alarm/translate.py
+++ custom_components/wetter_alarm/translate.py
@@ -1,9 +1,11 @@
 import json
+import aiofiles
 import os
 
-def load_translations(lang):
+async def load_translations(lang):
     translations_path = f'custom_components/wetter_alarm/translations/{lang}.json'
     if os.path.exists(translations_path):
-        with open(translations_path, 'r') as file:
-            return json.load(file)
+        async with aiofiles.open(translations_path, 'r') as file:
+            content = await file.read()
+            return json.loads(content)
     return {}
