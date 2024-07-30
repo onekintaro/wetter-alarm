5d62d87719808d6aeaa4384feffe5557d2f57a50
onekintaro <kintaro221@gmail.com>
2024-07-30 10:05:32 +0200

    Fix Issue: #1 in weather.py (should now Update weather data correctly 🌞)

--- custom_components/wetter_alarm/weather.py
+++ custom_components/wetter_alarm/weather.py
@@ -1,6 +1,7 @@
 """Support for Wetter-Alarm weather service."""
 
 import logging
+
 from homeassistant.components.weather import (
     WeatherEntity,
     WeatherEntityFeature,
@@ -11,7 +12,7 @@ from homeassistant.const import (
     UnitOfSpeed,
     UnitOfTemperature,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.util import slugify
@@ -19,10 +20,9 @@ from homeassistant.util import slugify
 from .const import (
     ATTRIBUTION,
     DOMAIN,
-    WETTERALARM_COORDINATOR,
-    WETTERALARM_DATA,
+    WETTERALARM_COORDINATOR
 )
-from .consts.devices import DeviceConst
+from .helpers.const.devices import DeviceConst
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,17 +33,17 @@ async def async_setup_entry(
     """Add a weather entity from a config_entry."""
     hass_data = hass.data[DOMAIN][entry.entry_id]
     coordinator = hass_data[WETTERALARM_COORDINATOR]
-    wetteralarm_data = hass_data[WETTERALARM_DATA]["weather_data"]
+    wetteralarm_data = coordinator.data["weather_data"]
     weather_device = coordinator.data['devices'][DeviceConst.WEATHER]
     if weather_device["active"] and wetteralarm_data:
         weather_entry = weather_device['entries'].get('weather', [])
         if weather_entry:
-            async_add_entities([WetterAlarmWeather(entry.data, wetteralarm_data, coordinator)], False)
+            async_add_entities([WetterAlarmWeather(wetteralarm_data, coordinator)], False)
 
 class WetterAlarmWeather(WeatherEntity):
     """Implementation of Wetter-Alarm weather."""
 
-    def __init__(self, entry_data, wetteralarm_data, coordinator):
+    def __init__(self, wetteralarm_data, coordinator):
         """Initialize the platform with a data instance and site."""
         self._wetteralarm_data = wetteralarm_data
         self._coordinator = coordinator
@@ -80,6 +80,16 @@ class WetterAlarmWeather(WeatherEntity):
     def supported_features(self):
         """Return the supported features."""
         return WeatherEntityFeature.FORECAST_HOURLY | WeatherEntityFeature.FORECAST_DAILY
+    
+    # @property
+    # def is_daytime(self):
+    #     """Return if the sun is currently up."""
+    #     current_symbol = self._wetteralarm_data["current_ext_data"]["symbol"]
+    #     _LOGGER.error(f"Current symbol: {current_symbol}")
+    #     #if current_symbol is negative integer, it is night time, otherwise it is day time
+    #     if current_symbol < 0:
+    #         return False
+    #     return True
 
     @property
     def native_temperature(self):
@@ -128,5 +138,16 @@ class WetterAlarmWeather(WeatherEntity):
     async def async_added_to_hass(self) -> None:
         """Connect to dispatcher listening for entity data notifications."""
         self.async_on_remove(
-            self._coordinator.async_add_listener(self.async_write_ha_state)
+            self._coordinator.async_add_listener(self._handle_coordinator_update)
         )
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Disconnect dispatcher listening for entity data notifications."""
+        self._coordinator.async_remove_listener(self._handle_coordinator_update)
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._wetteralarm_data = self._coordinator.data["weather_data"]
+        #_LOGGER.error(f"Updating weather data: {self._wetteralarm_data}")
+        self.async_write_ha_state()
